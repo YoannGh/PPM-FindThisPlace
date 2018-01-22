c1fcc8b8378bd950993f65b98c72003d012c4d57
YoannGh <yoann.ghigoff@gmail.com>
2018-01-22 10:20:30 +0100

    started score View

--- app/src/main/java/fr/upmc/m2sar/findthisplace/GameMapActivity.java
+++ app/src/main/java/fr/upmc/m2sar/findthisplace/GameMapActivity.java
@@ -3,6 +3,7 @@ package fr.upmc.m2sar.findthisplace;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -17,6 +18,6 @@ public class GameMapActivity extends FragmentActivity implements OnMapReadyCallb
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-
+        Toast.makeText(this, "Ceci est une map", Toast.LENGTH_LONG);
     }
 }
---
+++ app/src/main/java/fr/upmc/m2sar/findthisplace/ScoreActivity.java
@@ -0,0 +1,14 @@
+package fr.upmc.m2sar.findthisplace;
+
+import android.app.ListActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScoreActivity extends ListActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
